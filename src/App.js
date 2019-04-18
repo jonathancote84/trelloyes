a6d4238ddfbfe7bd9c96d3efe7117c83e9360f1b
jonathancote84 <jonathancote92@gmail.com>
2019-04-18 09:11:55 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import STORE from './STORE';
+import List from './List'
+import './App.css';
+
+
+
+function App() {
+  return(
+    <main className='App'>
+      <header className='App-header'>
+        <h1>Trelloyes!</h1>
+      </header>
+      <div className='App-list'>
+        {STORE.lists.map(list => (
+          <List
+            key={list.id}
+            header={list.header}
+            cards={list.cardIds.map(id => STORE.allCards[id])}
+          />
+        ))}
+      </div>
+    </main>
+  ); 
+}
+
+
+export default App;
